a0e314d56c97203cc832aa1a54a32c802ee73bb3
Deric Cheng <dericc@princeton.edu>
2013-06-11 21:44:24 -0700

    Working picture taker

--- GeoSnapProject/GeoSnap/src/main/java/com/opencagedc/geosnap/MainActivity.java
+++ GeoSnapProject/GeoSnap/src/main/java/com/opencagedc/geosnap/MainActivity.java
@@ -1,23 +1,101 @@
 package com.opencagedc.geosnap;
 
+import android.content.Intent;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 public class MainActivity extends Activity {
+    private final static String TAG = "SNAPPY";
+    private final static int SNAP_IMAGE_REQUEST_CODE = 10;
+
+    private Button button;
+    private ImageView imageView;
+    private Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(snapIt);
+        imageView = (ImageView) findViewById(R.id.image_view);
     }
 
+    OnClickListener snapIt = new OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            fileUri = getOutputImageFileUri();
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(intent, SNAP_IMAGE_REQUEST_CODE);
+
+            button.setOnClickListener(shareIt);
+            //button.setText(R.string.share_it);
+
+
+        }
+    };
+    OnClickListener shareIt = new OnClickListener() {
+        public void onClick(View v) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("image/jpeg");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+            startActivity(Intent.createChooser(shareIntent, "Share Image With"));
+        }
+    };
+
+    private Uri getOutputImageFileUri() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Snappy");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.e(TAG, "Couldn't create output directory");
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File file = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+        return Uri.fromFile(file);
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SNAP_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                Log.i(TAG, "Picture saved to " + fileUri.getPath());
+
+                System.out.println(fileUri.getPath());
+
+                try{
+                    ExifInterface exifPic = new ExifInterface(fileUri.getPath());
+                    float[] location = new float[2];
+                    boolean success = exifPic.getLatLong(location);
+
+                    Log.i(TAG, success+"");
+                    Log.i(TAG, Arrays.toString(location)+"");
+
+
+                }catch(Exception e){
+                    e.printStackTrace();
+                };
+
+
+
+            }
+        }
     }
-    
 }
